18b19351d0ca3f8d44c4d323776c2b5ee7c8e66e
sleekLancelot <sleekzy7@gmail.com>
2023-02-12 11:18:48 +0100

    integrate popup

--- src/Component/Map.tsx
+++ src/Component/Map.tsx
@@ -1,8 +1,9 @@
-import { Box, useToast } from '@chakra-ui/react'
+import { Box, Divider, Tag, Text, useToast, VStack, chakra } from '@chakra-ui/react'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Map, { 
     Marker,
     NavigationControl,
+    Popup,
 } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { cities } from '../constant';
@@ -10,18 +11,26 @@ import { Geocoder } from './Geocoder';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 
+interface CityProp {
+    name: string,
+    code: string,
+    latitude: number,
+    longitude: number,
+}
+
 const MapContainer = ({selectedCity}: {
-    selectedCity: {
-        name: string,
-        code: string,
-        latitude: number,
-        longitude: number,
-    }
+    selectedCity: CityProp
 }) => {
     const [viewport, setViewport] = useState({
         longitude: 0,
         latitude: 0,
         zoom: 8,
+    });
+    const [popupInfo, setPopupInfo] = useState<CityProp | any>({});
+    const [weatherDetails, setWeatherDetails] = useState(null);
+    const [apiResponse, setApiResponse] = useState({
+        status: false,
+        message: '',
     });
 
     const toast = useToast()
@@ -71,9 +80,15 @@ const MapContainer = ({selectedCity}: {
     }, []);
 
     const markers = useMemo(() => cities.map((city,index) => (
-        <Marker key={index}
+        <Marker
+            key={index}
             longitude={city.longitude}
-            latitude={city.latitude}>
+            latitude={city.latitude}
+            onClick={e => {
+                e.originalEvent.stopPropagation();
+                setPopupInfo(() => city);
+            }}
+        >
         </Marker>)
     ), [cities]);
 
@@ -105,6 +120,35 @@ const MapContainer = ({selectedCity}: {
                 }}
             />
             <Geocoder navigateTo={navigatorSuccess} />
+            {
+                popupInfo?.latitude !== undefined &&
+                popupInfo?.longitude !== undefined &&
+                <Popup
+                    anchor="top"
+                    longitude={Number(popupInfo?.longitude)}
+                    latitude={Number(popupInfo?.latitude)}
+                    onClose={() => setPopupInfo(() => {})}
+                >
+                    <Box
+                        w={220}
+                        h={200}
+                    >
+                        <VStack>
+                            <chakra.h2
+                                width={'100%'}
+                                color={'#000'}
+                                display={'flex'}
+                                justifyContent={'left'}
+                                fontSize={22}
+                                px={4}
+                                pb={2}
+                                borderBottom={'1px solid black'}
+                            >{popupInfo?.name}</chakra.h2>
+                            <Divider orientation='horizontal' />
+                        </VStack>
+                    </Box>
+                </Popup>
+            }
         </Map>
     </Box>
   )
